1f4aa2e634ad205848a9857f625123dd8fb690dc
Franco Rudy <rudyxptrent@hotmail.com>
2023-02-14 11:05:36 -0300

    Test Chrome is already functional

--- FirstTC/TC_1_Chrome.py
+++ FirstTC/TC_1_Chrome.py
@@ -1,5 +1,6 @@
-# Test Case
-# installation python path: C:\Users\RudyX\AppData\Local\Programs\Python
+# default python path: C:\Users\RudyX\AppData\Local\Programs\Python
+
+# packages
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -9,6 +10,8 @@ from selenium.webdriver.common.by import By
 # serv_obj=Service("C:\Pycharm2023\webdriver\chromedriver")
 # driverChrome=webdriver.Chrome(service=serv_obj)
 
+# Selenium 4 - Chrome
+
 # Without specified webdriver location
 driverChrome=webdriver.Chrome()
 driverChrome.get("https://www.google.com/?hl=es")
@@ -17,16 +20,7 @@ driverChrome.get("https://www.google.com/?hl=es")
 # options.page_load_strategy='eager'
 driverChrome.implicitly_wait(20)
 
-# Opcion 1 ----------------------------------------
-# txtSearch = driverChrome.find_element(by=By.NAME, value="q")
-# btnSearch = driverChrome.find_element(by=By.NAME, value="btnK")
-#
-# txtSearch.send_keys("Maroon 5")
-# btnSearch.click()
-#
-# print(driverChrome.title)
 
-# Opcion 2 ----------------------------------------
 driverChrome.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 driverChrome.implicitly_wait(20)
 
@@ -34,9 +28,17 @@ txtUser = driverChrome.find_element(by=By.XPATH, value="//Input[@name='username'
 txtPass = driverChrome.find_element(by=By.XPATH, value="//Input[@name='password']")
 btnLogin = driverChrome.find_element(by=By.XPATH, value="//button[@type='submit']")
 
-txtUser.send_keys("Maroon 5")
-txtPass.send_keys("Admin")
+txtUser.send_keys("Admin")
+txtPass.send_keys("admin123")
 btnLogin.click()
 
+titlePage = driverChrome.title
+
+if titlePage == "OrangeHRM":
+    print("Test Passed")
+else:
+    print("Test Failed")
+
 # Print the tittle of the page
-print(driverChrome.title)
+print(titlePage)
+driverChrome.quit()
