6a58f037b1700d8869b3409c3d1d115264cef030
Franco Rudy <rudyxptrent@hotmail.com>
2023-02-16 10:57:45 -0300

    Revert "TC for each browser (Firefox, Chrome and Edge)"
    This reverts commit b08df06e1e059cdc83006cf128f54ee14b1f7106.

--- FirstTC/TC_1_Chrome.py
+++ FirstTC/TC_1_Chrome.py
@@ -3,9 +3,6 @@
 # packages
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.ie.webdriver import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
@@ -15,74 +12,33 @@ from selenium.webdriver.common.by import By
 
 # Selenium 4 - Chrome
 
+# Without specified webdriver location
+driverChrome=webdriver.Chrome()
+driverChrome.get("https://www.google.com/?hl=es")
+
 # options=Options()
 # options.page_load_strategy='eager'
-# driverChrome.implicitly_wait(20)
+driverChrome.implicitly_wait(20)
 
-executeTC = 2
 
-if executeTC==1:
-    # Without specified webdriver location
-    driverChrome = webdriver.Chrome()
+driverChrome.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driverChrome.implicitly_wait(20)
 
-    driverChrome.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    driverChrome.implicitly_wait(20)
+txtUser = driverChrome.find_element(by=By.XPATH, value="//Input[@name='username']")
+txtPass = driverChrome.find_element(by=By.XPATH, value="//Input[@name='password']")
+btnLogin = driverChrome.find_element(by=By.XPATH, value="//button[@type='submit']")
 
-    txtUser = driverChrome.find_element(by=By.XPATH, value="//Input[@name='username']")
-    txtPass = driverChrome.find_element(by=By.XPATH, value="//Input[@name='password']")
-    btnLogin = driverChrome.find_element(by=By.XPATH, value="//button[@type='submit']")
+txtUser.send_keys("Admin")
+txtPass.send_keys("admin123")
+btnLogin.click()
 
-    txtUser.send_keys("Admin")
-    txtPass.send_keys("admin123")
-    btnLogin.click()
+titlePage = driverChrome.title
 
-    titlePage = driverChrome.title
-    if titlePage == "OrangeHRM":
-        print("Test Passed")
-    else:
-        print("Test Failed")
-    driverChrome.quit()
-
-elif executeTC==2:
-    driverEdge = webdriver.Edge()
-
-    driverEdge.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    driverEdge.implicitly_wait(20)
-
-    txtUser = driverEdge.find_element(by=By.XPATH, value="//Input[@name='username']")
-    txtPass = driverEdge.find_element(by=By.XPATH, value="//Input[@name='password']")
-    btnLogin = driverEdge.find_element(by=By.XPATH, value="//button[@type='submit']")
-
-    txtUser.send_keys("Admin")
-    txtPass.send_keys("admin123")
-    btnLogin.click()
-
-    titlePage = driverEdge.title
-    if titlePage == "OrangeHRM":
-        print("Test Passed")
-    else:
-        print("Test Failed")
-    driverEdge.quit()
+if titlePage == "OrangeHRM":
+    print("Test Passed")
 else:
-    driverFirefox = webdriver.Firefox()
-
-    driverFirefox.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    driverFirefox.implicitly_wait(20)
-
-    txtUser = driverFirefox.find_element(by=By.XPATH, value="//Input[@name='username']")
-    txtPass = driverFirefox.find_element(by=By.XPATH, value="//Input[@name='password']")
-    btnLogin = driverFirefox.find_element(by=By.XPATH, value="//button[@type='submit']")
-
-    txtUser.send_keys("Admin")
-    txtPass.send_keys("admin123")
-    btnLogin.click()
-
-    titlePage = driverFirefox.title
-    if titlePage == "OrangeHRM":
-        print("Test Passed")
-    else:
-        print("Test Failed")
-    driverFirefox.quit()
+    print("Test Failed")
 
 # Print the tittle of the page
 print(titlePage)
+driverChrome.quit()
