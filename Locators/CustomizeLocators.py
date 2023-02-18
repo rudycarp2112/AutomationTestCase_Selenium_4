757671c81adc1084aea2e5463cce13ab7fdc4b18
Franco Rudy <rudyxptrent@hotmail.com>
2023-02-18 09:39:01 -0300

    Second Class where i learned more about locators in selenium

---
+++ Locators/CustomizeLocators.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service_obj=Service("C:\Pycharm2023\webdriver\chromedriver")
+driver=webdriver.Chrome(service=service_obj)
+
+driver.get("https://www.facebook.com/?stype=lo&jlou=AferocbhzszZvg2vBBqOrtMADeS5i8TnGZ-vroGnpxQA7KGTqisku1isClsR38QzMmIDW_XHH6rBKBs4TnG5ApOQLM5FQHCaCkILoDyW8N-_wQ&smuh=37133&lh=Ac9NrOU87PHG1MvV1Oc")
+
+# tag and name combination -> sintax: tagname#nameValue
+# driver.find_element(By.CSS_SELECTOR, value="#email").send_keys("franco.rudy@hotmail.com")
+# driver.find_element(By.CSS_SELECTOR, value="input#email").send_keys("franco.rudy@hotmail.com")
+
+# tag and class combination -> sintax: tagname.classValue
+# driver.find_element(By.CSS_SELECTOR, value=".inputtext").send_keys("franco.rudy@hotmail.com") #without TAG
+# driver.find_element(By.CSS_SELECTOR, value="input.inputtext").send_keys("franco.rudy@hotmail.com")
+
+# tag and attribute combination -> sintax: tagname[attribute=attributeValue]
+# driver.find_element(By.CSS_SELECTOR, value="input[data-testid=royal_email]").send_keys("franco.rudy@hotmail.com")
+# driver.find_element(By.CSS_SELECTOR, value="[data-testid=royal_email]").send_keys("franco.rudy@hotmail.com")
+
+# tag, class and attribute combination -> sintax: tagname.classValue[attribute=attributeValue]
+driver.find_element(By.CSS_SELECTOR, value="input.inputtext[data-testid=royal_email]").send_keys("franco.rudy@hotmail.com")
+driver.find_element(By.CSS_SELECTOR, value="input.inputtext[data-testid=royal_pass]").send_keys("1234569")
---
+++ Locators/Second_Class.py
@@ -0,0 +1,38 @@
+# https://youtu.be/4DHefIyw6ik?list=PLUDwpEzHYYLsuUBvuoYTlN0KsBB5t-BDa&t=1970
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+options = Options()
+options_page_load_strategy= "normal"
+webdriverChrome=webdriver.Chrome(options=options)
+
+webdriverChrome.get("https://demo.nopcommerce.com")
+webdriverChrome.maximize_window()
+
+## Unique Locators
+# locators to find one element (more of time) - properties: ID, NAME, LINK_TEXT, PARTIAL_LINK_TEXT
+# webdriverChrome.find_element(By.ID, value="small-searchterms").send_keys("galaxy")
+# webdriverChrome.find_element(By.NAME, value="q").send_keys("galaxy")
+# webdriverChrome.find_element(By.LINK_TEXT, value="Register").click()
+# webdriverChrome.find_element(By.PARTIAL_LINK_TEXT, value="ister").click()
+
+# locators to find one or MORE elements (more of time, example: five radio buttons)
+# slides = webdriverChrome.find_elements(By.CLASS_NAME, value="nivo-imageLink")
+# print(len(slides))  # 2 total images
+
+# links = webdriverChrome.find_elements(By.TAG_NAME, value="a")
+# print(len(links)) # 90 total links
+
+## Customized Locators
+
+# CSS_SELECTOR
+# XPATH
+
+print(webdriverChrome.title)
+
+#webdriverChrome.close() # close one of all webdrivers open
+#webdriverChrome.quit() # all webdrivers open
