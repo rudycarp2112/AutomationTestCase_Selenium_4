fad9aca05634f21b0ce83e0d660726591e4b703b
Franco Rudy <rudyxptrent@hotmail.com>
2023-04-01 20:10:03 -0300

    I upload some files that are necessary to execute automatized TC from jenkins

---
+++ NopCommerceApp_Hybrid_FW/run.bat
@@ -0,0 +1,13 @@
+REM chrome
+
+pytest -s -v -m "sanity" --html=Reports/reportChrome.html ./testCases/ --browser chrome
+pytest -s -v -m "sanity" --html=Reports/reportFirefox.html ./testCases/ --browser firefox
+
+REM pytest -s -v -m "sanity and regression" --html=Reports/report.html ./testCases/ --browser chrome
+REM pytest -s -v -m "sanity or regression" --html=Reports/report.html ./testCases/ --browser chrome
+REM pytest -s -v -m "regression" --html=Reports/report.html ./testCases/ --browser chrome
+
+
+REM pytest -s -v -m "sanity and regression" --html=Reports/report.html ./testCases/ --browser firefox
+REM pytest -s -v -m "sanity or regression" --html=Reports/report.html ./testCases/ --browser firefox
+REM pytest -s -v -m "regression" --html=Reports/report.html ./testCases/ --browser firefox
